78e84c102a33b30b58f86d90ae3c2f9aa217a741
seanbsm <Guild_wars100@hotmail.com>
2016-12-13 16:42:40 +0100

    started entering Minnesota potential, not done, edit 1

--- CCD_prototype/CCD/Systems/mp.cpp
+++ CCD_prototype/CCD/Systems/mp.cpp
@@ -1,5 +1,108 @@
 #include "mp.h"
+#include <iostream>
 
-MP::MP() /* Minnesota Potential */
+using namespace std;
+
+MP::MP(Master* master, double m, double L3, double L2, double L1) : System(master) /* Minnesota Potential */
 {
+    m_Nh = master->m_Nh;
+    m_Nb = master->m_Nb;
+    m_master = master;
+
+    m_m  = m;
+    m_L3 = L3;
+    m_L2 = L2;
+    m_L1 = L1;
+    makeStateSpace();
+}
+
+void MP::makeStateSpace(){
+    m_states.conservativeResize(m_states.rows()+6, Eigen::NoChange);    //sets the format of states
+    //start for-loops
+    for (int n2=0; n2<m_Nb+1; n2++){
+        for (int nx=-n2; nx<n2+1; nx++){
+            for (int ny=-n2; ny<n2+1; ny++){
+                for (int nz=-n2; nz<n2+1; nz++){
+                    if (nx*nx + ny*ny + nz*nz == n2){
+                        m_states.conservativeResize(Eigen::NoChange, m_states.cols()+2);
+                        m_states.col(m_states.cols()-4) << n2,nx,ny,nz, 1, 1;
+                        m_states.col(m_states.cols()-3) << n2,nx,ny,nz, 1,-1;
+                        m_states.col(m_states.cols()-2) << n2,nx,ny,nz,-1, 1;
+                        m_states.col(m_states.cols()-1) << n2,nx,ny,nz,-1,-1;
+                    }
+                }
+            }
+        }
+    }
+
+    m_master->m_Ns = m_states.cols();
+    below_fermi = Eigen::VectorXi::LinSpaced(m_Nh,0,m_Nh);
+    above_fermi = Eigen::VectorXi::LinSpaced(m_Ns,m_Nh,m_Ns);
+}
+
+//I think using eigen here is a bit over-the-top for such a function, but whatevs~
+int MP::kUnique2(int k, int p){
+    Eigen::Vector4i kk( m_states(1,k), m_states(2,k), m_states(3,k), m_states(4,k) );
+    Eigen::Vector4i kp( m_states(1,p), m_states(2,p), m_states(3,p), m_states(4,p) );
+    Eigen::VectorXi mom = kk+kp;
+
+    int val = 0;
+    for (int i = 0; i<mom.rows();i++){
+        if (val < mom(i)){
+            val = mom(i);
+        }
+    }
+
+    int dk = 2*val + 1;
+    int kuni = mom(0) + mom(1)*dk + mom(2)*dk*dk + mom(3)*dk*dk*dk;
+    return kuni;
+}
+
+double MP::f(int p){
+    double returnVal = h0(p);
+    for (int i=0; i<below_fermi.rows(); i++){
+        returnVal += assym(p,below_fermi(i),p,below_fermi(i));
+    };
+    return returnVal;
+}
+
+double MP::h0(int p){
+    double energy = m_states(p,0);
+    return energy*2*pi*pi/(m_m*m_L2);
+}
+
+double MP::assym(int p, int q, int r, int s){
+
+    Eigen::Vector3i kp( m_states(1,p), m_states(2,p), m_states(3,p) );
+    Eigen::Vector3i kq( m_states(1,q), m_states(2,q), m_states(3,q) );
+    Eigen::Vector3i kr( m_states(1,r), m_states(2,r), m_states(3,r) );
+    Eigen::Vector3i ks( m_states(1,s), m_states(2,s), m_states(3,s) );
+    int sp = m_states(4,p); int tp = m_states(5,p);
+    int sq = m_states(4,q); int tq = m_states(5,q);
+    int sr = m_states(4,r); int tr = m_states(5,r);
+    int ss = m_states(4,s); int ts = m_states(5,s);
+
+    if ( vecDelta(kp+kq, kr+ks) ){
+        double returnVal = 0;
+        return 0;
+    }
+    else{
+        return 0;
+    }
+}
+
+bool MP::vecDelta(Eigen::VectorXi v1, Eigen::VectorXi v2){
+    int dim1 = v1.rows();
+    int dim2 = v2.rows();
+    if (dim1 != dim2){
+        cout << "dimensional error" << endl;
+        return 0;
+    }
+    else{
+        bool returnInt = 1;
+        for (int i =0; i<dim1; i++){
+            returnInt *= ( v1(i)==v2(i) );
+        }
+        return returnInt;
+    }
 }
--- CCD_prototype/CCD/Systems/mp.h
+++ CCD_prototype/CCD/Systems/mp.h
@@ -1,15 +1,46 @@
 #ifndef MP_H
 #define MP_H
 
-class MP
+#include "master.h"
+#include "system.h"
+#include <math.h>
+#include <cmath>
+#include <eigen3/Eigen/Dense>
+
+class MP : public System
 {
+private:
+    bool vecDelta(Eigen::VectorXi v1, Eigen::VectorXi v2);
+
+    const double V_0R    = 200;    //MeV
+    const double V_0T    = 178;    //MeV
+    const double V_0S    = 91.85;  //MeV
+    const double kappa_R = 1.487;  //fm^-2
+    const double kappa_T = 0.639;  //fm^-2
+    const double kappa_S = 0.465;  //fm^-2
+
 public:
-    MP();
-    double  makeStateSpace  ();
+    double          pi     = M_PI;
+    double          m_m    = 0;
+    double          m_L3   = 0;
+    double          m_L2   = 0;
+    double          m_L1   = 0;
+
+    int             m_Nh   = 0;
+    int             m_Nb   = 0;
+    int             m_Ns   = 0;
+
+    Eigen::VectorXi below_fermi;
+    Eigen::VectorXi above_fermi;
+    Eigen::MatrixXi m_states;
+
+    MP(class Master* master, double m, double L3, double L2, double L1);
+    class   Master* m_master = nullptr;
+    void    makeStateSpace  ();
     double  assym           (int p, int q, int r, int s);
     double  h0              (int p);
     double  f               (int p);
-    int     kunique2        (int p, int q);
+    int     kUnique2        (int p, int q);
 };
 
 #endif // MP_H
